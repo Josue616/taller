bac9e0edb65e4b1344171ce6397215552d28eafe
Josue616 <ja2020067149@virtual.upt.pe>
2024-05-04 13:24:22 -0500

    errors

--- lib/Controller/user_controller.dart
+++ lib/Controller/user_controller.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class UserController {
-  final String baseUrl = 'http://localhost:600';
+  final String baseUrl = 'http://161.132.37.95:600';
 
   Future<String> authenticateUser(String username, String password) async {
     try {
@@ -14,11 +14,37 @@ class UserController {
 
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
-        return data['success']
-            ? 'Ingreso correcto'
-            : 'Credenciales incorrectas';
+        if (data['success']) {
+          return 'Ingreso correcto';
+        } else {
+          return data['message'] ?? 'Credenciales incorrectas';
+        }
+      } else if (response.statusCode == 401) {
+        return 'Usuario o contraseña incorrectos';
       } else {
-        return 'Error: ${response.statusCode}';
+        return 'Error de conexión: ${response.statusCode}';
+      }
+    } catch (e) {
+      return 'Error de red: Por favor verifica tu conexión a internet';
+    }
+  }
+
+  Future<String> registerUser(String username, String password) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$baseUrl/register'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'username': username, 'password': password}),
+      );
+
+      if (response.statusCode == 201) {
+        return 'Registro exitoso';
+      }
+      if (response.statusCode == 409) {
+        return 'El nombre de Usuario ya esta tomado';
+      } else {
+        var data = jsonDecode(response.body);
+        return data['message'] ?? 'Error durante el registro';
       }
     } catch (e) {
       return 'Error: $e';
